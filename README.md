41e081f20b9baedf6f18f6fe55afbf81e0178486
Filippo Veneri <filippo.veneri@apconsulting.net>
2024-07-11 11:41:27 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,51 @@
+iriswagui
+=========
+
+This package allows the user to easily add a [Swagger UI](https://github.com/swagger-api/swagger-ui) to an [Iris](https://www.iris-go.com/) application.
+It does so by embedding the Swagger UI assets and adding a party to a parent party that implements all the necessary endpoints.
+
+Basic usage example:
+```go
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/alchemy/iriswagui"
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	specPath := flag.String("spec", "data/swagger.json", "specification file `path`")
+	addr := flag.String("addr", "127.0.0.1:8888", "listening address `host:port`")
+
+	app := iris.New()
+	party := app.Party("/documentation")
+
+	config := iriswagui.Config{
+		SpecRefs: []iriswagui.SwaggerSpecRef{
+			{Name: "MyAPI", URL: *specPath},
+		},
+	}
+    // Swagger UI will be served from /documentation/swagger
+	err := iriswagui.HandleSwaggerUI(party, "/swagger", config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = app.Listen(*addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+```
+
+Swagger UI configuration
+------------------------
+Swagger UI is highly customizable. This package Config struct offers only minimal configuration optios.  
+For instance is accepts only a list of specification files and wether to use deep linking or not.  
+If only one specification file is added, Swagger UI will be configured to use `BasicLayout`, otherwise `StandaloneLayout` will be used along with the `SwaggerUIBundle.plugins.Topbar` plugin.  
+However a complete custom configration can be specified by using the `ExternalConfigURL` field og the `Config` struct. When this field is not empty
+the configuration object will be fetched from this URL, ignoring all other fields.
+
---
+++ iriswagui.go
@@ -0,0 +1,111 @@
+package iriswagui
+
+import (
+	"embed"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"text/template"
+
+	"github.com/kataras/iris/v12"
+)
+
+//go:embed swagger-ui
+var swaggerUI embed.FS
+
+var indexTmpl *template.Template
+var swaggerInitTmpl *template.Template
+
+func init() {
+	var err error
+	indexTmpl, err = template.ParseFS(swaggerUI, "swagger-ui/index.html.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	swaggerInitTmpl, err = template.ParseFS(swaggerUI, "swagger-ui/swagger-initializer.js.tmpl")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// SwaggerSpecRef represents a single swagger specification file.
+// The URL field can be and absolute URL or a local path. In the
+// letter case a route to serve that file will be added to the
+// Swagger UI party.
+type SwaggerSpecRef struct {
+	Name string
+	URL  string
+}
+
+// Config represents a (minimal) swagger UI configuration.
+type Config struct {
+	// ExternalConfigURL is the URL of an external configuration JavaScript object.
+	// If it is non empty the UI configuration will be base on this object only, all
+	// other parameters will be ingored.
+	ExternalConfigURL string
+	// SpecRefs is a list of API specifications [SwaggerSpecRef].
+	// If only one SpecRef is present Swagger UI will be configured with the BasicLayout,
+	// otherwise with the StandaloneLayout and the SwaggerUIBundle.plugins.Topbar plugin.
+	SpecRefs []SwaggerSpecRef
+	// DeepLinking will determine the value of the corresponfing parameter
+	// of the Swagger UI configuration.
+	DeepLinking bool
+}
+
+type swaggerUIConfig struct {
+	Config
+	BaseURL string
+}
+
+// HandleSwaggerUI adds a new sub-party to party with all the necessary endpoints
+// to serve the Swagger UI based on config.
+func HandleSwaggerUI(party iris.Party, relativePath string, config Config) error {
+	swaggerParty := party.Party(relativePath)
+	swaggerParty.HandleDir("/", swaggerUI, iris.DirOptions{IndexName: "/index.html", Compress: true})
+	uiConfig := &swaggerUIConfig{
+		Config:  config,
+		BaseURL: swaggerParty.GetRelPath(),
+	}
+
+	specsParty := swaggerParty.Party("/specs")
+	for i, specRef := range uiConfig.SpecRefs {
+		parsedUrl, err := url.Parse(specRef.URL)
+		if err != nil {
+			return err
+		}
+		if !parsedUrl.IsAbs() {
+			base := path.Base(parsedUrl.Path)
+			relPath := "/" + base
+			specsParty.Get(relPath, func(ctx iris.Context) {
+				specContent, err := os.ReadFile(parsedUrl.Path)
+				if err != nil {
+					ctx.StopWithError(http.StatusInternalServerError, err)
+				}
+				ctx.Write(specContent)
+			})
+			specsPartyRelPath := specsParty.GetRelPath()
+			uiConfig.SpecRefs[i].URL, err = url.JoinPath(specsPartyRelPath, relPath)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	swaggerParty.Get("/index.html", func(ctx iris.Context) {
+		err := indexTmpl.ExecuteTemplate(ctx.ResponseWriter(), "index.html.tmpl", uiConfig)
+		if err != nil {
+			ctx.StopWithError(http.StatusInternalServerError, err)
+		}
+	})
+
+	swaggerParty.Get("/swagger-initializer.js", func(ctx iris.Context) {
+		err := swaggerInitTmpl.ExecuteTemplate(ctx.ResponseWriter(), "swagger-initializer.js.tmpl", uiConfig)
+		if err != nil {
+			ctx.StopWithError(http.StatusInternalServerError, err)
+		}
+	})
+
+	return nil
+}
